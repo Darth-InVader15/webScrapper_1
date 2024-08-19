283aa451f215233d01afb4478262a114c21ca6b8
Darth-InVader15 <piyush.singh1315@gmail.com>
2024-08-20 01:12:45 +0530

    fix: Issue with lazy loading

--- testing/spiders/productspider.py
+++ testing/spiders/productspider.py
@@ -2,18 +2,24 @@ import scrapy
 
 class ProductSpider(scrapy.Spider):
     name = "prods"
-    start_urls = ['https://nobero.com/collections/men-oversized-t-shirts']
+    start_urls = ['https://nobero.com/collections/men-oversized-t-shirts?page=1']
 
     def parse(self, response):
-        for link in response.css('a.product_link'):
-         # Check if link exists before accessing attrib
-            if link:
-                yield {
-                'link': link.attrib['href']
-                }
-            # yield response.follow(link.get(), self.parse_product)
+        product_links = response.css('a.product_link')
+        
+        if not product_links:
+            return
 
-        # yield {
-        #         'name': response.css("h1.capitalize.text-lg.md:text-[1.375rem].font-[familySemiBold].leading-none::text")
-        #     }
-    
+        for link in product_links:
+            yield {
+                'link': link.attrib['href']
+            }
+
+        # Get the current page number from the URL
+        current_page = int(response.url.split('=')[-1])
+
+        # Construct the URL of the next page
+        next_page = f'https://nobero.com/collections/men-oversized-t-shirts?page={current_page + 1}'
+
+        # Yield a request to the next page
+        yield scrapy.Request(next_page, self.parse)
